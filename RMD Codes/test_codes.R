fa0b11696c270a9a00ae89d4e1636fa834f1e56a
saianjesh <saianjesh@aol.com>
2024-03-11 22:39:57 -0500

    including necessary files and codes

---
+++ RMD Codes/test_codes.R
@@ -0,0 +1,9 @@
+# write.csv(x = adult_data, 
+#           file = "test_adult_data.csv", 
+#           col.names = TRUE, 
+#           row.names = FALSE)
+
+write.csv(x = student_mat_data, 
+          file = "test_student_mat_data.csv", 
+          col.names = TRUE, 
+          row.names = FALSE)
