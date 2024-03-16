a008813a873f0570c5dbc25f45870025ecc6217e
saianjesh <saianjesh@aol.com>
2024-03-15 23:32:51 -0500

    Updated the Part 2 problem - need to complete the interpretation

--- RMD Codes/test_codes.R
+++ RMD Codes/test_codes.R
@@ -3,7 +3,24 @@
 #           col.names = TRUE, 
 #           row.names = FALSE)
 
-write.csv(x = student_mat_data, 
-          file = "test_student_mat_data.csv", 
-          col.names = TRUE, 
-          row.names = FALSE)
+# write.csv(x = student_mat_data, 
+#           file = "test_student_mat_data.csv", 
+#           col.names = TRUE, 
+#           row.names = FALSE)
+
+# df = student_mat_data
+# target_var = "G3"
+# v = "age"
+# plot(x = df[, target_var], 
+#      y = df[,v], 
+#      main = paste("Scatter Plot - ",target_var," vs. ",v, sep = ""),
+#      xlab = target_var, 
+#      ylab = v)
+# 
+# print(paste("Correlation between thee variables : ", cor(df[, target_var], df[, v])))
+# 
+# numer_Test_details <- cor.test(df[,v], df[,target_var], method="kendall")
+# print(numer_Test_details)
+
+
+
